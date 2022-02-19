076d0cacd44396da879e30151836faa54b3cd529
Tetyana <iltaev2010@gmail.com>
2022-02-18 18:03:25 -0600

    find rotation-count added

--- prob-divide-conquer/find-rotation-count.js
+++ prob-divide-conquer/find-rotation-count.js
@@ -6,21 +6,29 @@
 // Time Complexity: O(log N)
 
 // Examples:
-// findRotationCount([2, 3, 6, 12, 15, 18]) // -----
 // findRotationCount([15, 18, 2, 3, 6, 12]) // 2
 // findRotationCount([7, 9, 11, 12, 5]) // 4
 // findRotationCount([7, 9, 11, 12, 15]) // 0
 
 
-function findRotationCount(arr, num) {
-
-    
-
-
-
-
-
+function findRotationCount(arr) {
+    if(arr.length == 0 || arr == null) return -1;
+    // pivot search
+    let left = 0;
+    let right = arr.length-1;
+    while (left < right) {
+        let middle = Math.floor((left+right)/2);
+        if (arr[middle] > arr[right]) {
+            left = middle + 1; 
+        }
+        else { 
+            right = middle; 
+        } 
+    } 
+    // pivot = left 
+    return left;
 }
 
 module.exports = findRotationCount;
+
 
