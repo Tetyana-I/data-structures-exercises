9bda31f3c6187e2593c1a932621ba656979f099e
Tetyana <iltaev2010@gmail.com>
2022-03-01 18:43:52 -0600

    general tree finished

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -15,20 +15,73 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    // if tree is empty return 0;
+    if (this.root === null) return 0;
+    let toVisitQueue = [this.root];
+    let sum = 0;
     
+    // go through all nodes in a queue
+    while (toVisitQueue.length) {
+      // if queue is not empty takes a first node
+      let current = toVisitQueue.shift();
+      // validation: if value is a number adds to sum, otherwise skips it
+      if (!isNaN(current.val)) {
+        sum = sum + current.val;
+      }
+      // if a current node have children push them to the queue 
+      for (let child of current.children)
+        toVisitQueue.push(child);
+    }
+    return sum;
   }
+
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    // if tree is empty return 0;
+    if (this.root === null) return 0;
+    let toVisitQueue = [this.root];
+    let count = 0;
+    
+    // go through all nodes in a queue
+    while (toVisitQueue.length) {
+      // if queue is not empty takes a first node
+      let current = toVisitQueue.shift();
+      // validation: if value is a number and it's even increments count, otherwise skips it
+      if (!isNaN(current.val) && (current.val % 2 === 0)) {
+        count++;
+      }
+      // if a current node have children push them to the queue 
+      for (let child of current.children)
+        toVisitQueue.push(child);
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    // if tree is empty return 0;
+    if (this.root === null) return 0;
+    if (isNaN(lowerBound)) throw new Error("Invalif input");
+    let toVisitQueue = [this.root];
+    let count = 0;
+    
+    // go through all nodes in a queue
+    while (toVisitQueue.length) {
+      // if queue is not empty takes a first node
+      let current = toVisitQueue.shift();
+      // validation: if value is a number and it's even increments count, otherwise skips it
+      if (!isNaN(current.val) && (current.val > lowerBound)) {
+        count++;
+      }
+      // if a current node have children push them to the queue 
+      for (let child of current.children)
+        toVisitQueue.push(child);
+    }
+    return count;
   }
 }
 
