b5eabc288e200d641ebb5148e13a165e1a9fe1f4
Tetyana <iltaev2010@gmail.com>
2022-03-04 22:29:12 -0600

    added recursive DFS version

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -61,6 +61,19 @@ class Graph {
     return visitOrder;
   }
 
+  // this function returns an array of Node values using recursive DFS 
+  recursiveDFS(start, visited = new Set([start]), visitOrder = []) {
+    let current = start;
+    visitOrder.push(current.value);
+    for (let neighbour of current.adjacent) {
+      if (!visited.has(neighbour)) {
+        visited.add(neighbour);
+        this.recursiveDFS(neighbour, visited, visitOrder);
+      }
+    }
+    return visitOrder;
+  }
+
   // this function returns an array of Node values using BFS
   breadthFirstSearch(start) {
     let toVisitQueue = [start];
@@ -82,49 +95,3 @@ class Graph {
 }
 
 module.exports = {Graph, Node};
-
-let graph = new Graph();
-let S = new Node("S");
-let P = new Node("P");
-let U = new Node("U");
-let X = new Node("X");
-let Q = new Node("Q");
-let Y = new Node("Y");
-let V = new Node("V");
-let R = new Node("R");
-let W = new Node("W");
-let T = new Node("T");
-
-graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
-
-graph.addEdge(S, P);
-graph.addEdge(S, U);
-
-graph.addEdge(P, X);
-graph.addEdge(U, X);
-
-graph.addEdge(P, Q);
-graph.addEdge(U, V);
-
-graph.addEdge(X, Q);
-graph.addEdge(X, Y);
-graph.addEdge(X, V);
-
-graph.addEdge(Q, R);
-graph.addEdge(Y, R);
-
-graph.addEdge(Y, W);
-graph.addEdge(V, W);
-
-graph.addEdge(R, T);
-graph.addEdge(W, T);
-
-// var result = JSON.stringify(graph.depthFirstSearch(S));
-// var validResult =
-//   result ===
-//     JSON.stringify(["S", "U", "V", "W", "T", "R", "Q", "Y", "X", "P"]) ||
-//   result ===
-//     JSON.stringify(["S", "P", "X", "U", "V", "W", "Y", "R", "Q", "T"]);
-// console.log(validResult); //true
-
-console.log(graph.depthFirstSearch(S));
