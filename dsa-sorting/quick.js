3bdb15e45f775b73c6ce6834865633cbdf24e687
Tetyana <iltaev2010@gmail.com>
2022-03-07 00:51:51 -0600

    selection and pivot

--- dsa-sorting/quick.js
+++ dsa-sorting/quick.js
@@ -3,7 +3,36 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+// It will help to accept three arguments:
+// an array, a start index, and an end index (these can default to 0 and the array length minus 1, respectively)
+// Grab the pivot from the start of the array;
+// Store the current pivot index in a variable (this will keep track of where the pivot should end up)
+// Loop through the array from the start until the end
+// If the pivot is greater than the current element,
+// increment the pivot index variable and then swap the current element with the element at the pivot index
+// Swap the starting element (i.e. the pivot) with the pivot index
+// Return the pivot index
+
+function pivot(arr, start=1, end=arr.length-1) {
+    let pivot = arr[0];
+    let pivotInd = 0;
+    while (start < end) {
+        if ((arr[start] > pivot) && (arr[end] < pivot)) {
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+        if (arr[start] < pivot) {
+            pivotInd = start;
+            start ++;
+        }
+        if (arr[end] > pivot) end --;
+    }
+    arr[0] = arr[pivotInd];
+    arr[pivotInd] = pivot;
+    return pivotInd;
+}
+
 
 /*
 quickSort accepts an array, left index, and right index
@@ -11,4 +40,4 @@ quickSort accepts an array, left index, and right index
 
 function quickSort() {}
 
-module.exports = quickSort;
+module.exports =  {quickSort, pivot};
--- dsa-sorting/selection.js
+++ dsa-sorting/selection.js
@@ -1,3 +1,29 @@
-function selectionSort() {}
+// Selection sort
+// Assign the first element to be the smallest value (this is called the minimum). 
+// (It does not matter right now if this actually the smallest value in the array.)
+// Compare this item to the next item in the array until you find a smaller number.
+// If a smaller number is found, designate that smaller number to be the new “minimum” and continue until the end of the array.
+// If the “minimum” is not the value (index) you initially began with, swap the two values. 
+// You will now see that the beginning of the array is in the correct order (similar to how after the first iteration of bubble sort, we know the rightmost element is in its correct place).
+// Repeat this with the next element until the array is sorted.
+
+function selectionSort(arr) {
+    for (let i=0; i < arr.length-1; i++) {
+        let min = arr[i];
+        let minInd = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                minInd = j;
+            } 
+        }
+        if (i !== minInd ) {
+            let temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
+        }  
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
