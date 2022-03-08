71ae2be4189ef0bfd019d35d37d2df3fcbb0e6e0
Tetyana <iltaev2010@gmail.com>
2022-03-07 19:01:45 -0600

    fixed bug in pivot

--- dsa-sorting/quick.js
+++ dsa-sorting/quick.js
@@ -12,32 +12,80 @@ You can assume the pivot is always the first element
 // increment the pivot index variable and then swap the current element with the element at the pivot index
 // Swap the starting element (i.e. the pivot) with the pivot index
 // Return the pivot index
+// import { merge } from './merge.js';
+const merge = require('./merge');
 
 function pivot(arr, start=1, end=arr.length-1) {
+    function swap (arr, a, b) {
+        [arr[a], arr[b]] = [arr[b], arr[a]];
+        }
+    if (arr.length < 2) return 0;
     let pivot = arr[0];
     let pivotInd = 0;
     while (start < end) {
-        if ((arr[start] > pivot) && (arr[end] < pivot)) {
-            let temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
+        // scenario 1: need to swap right and left values if left value is greater than pivot and right is smaller or equal pivot value
+        if ((arr[start] > pivot) && (arr[end] <= pivot)) {
+            swap(arr, start, end);
+            pivotInd = start;
+            start++;
+            end--;
         }
-        if (arr[start] < pivot) {
+        // scenario 2: need to move left pointer (pivot index) to the right if both left and right values are equal to pivot  
+        else if ((arr[start] === pivot) && (arr[end] === pivot)) {
             pivotInd = start;
             start ++;
         }
-        if (arr[end] > pivot) end --;
+        // scenario 3: need to move left pointer (pivot index) to the right if left value is less than pivot
+        else if (arr[start] < pivot) {
+            pivotInd = start;
+            start ++;
+        }
+        // scenario 4: need to move right pointer to the left if right value is greater than pivot 
+        else if (arr[end] > pivot) {
+            end --;
+        }
     }
-    arr[0] = arr[pivotInd];
-    arr[pivotInd] = pivot;
+    swap(arr,0,pivotInd);
+    console.log("arr after pivot:", arr, "pivot", pivotInd);    
     return pivotInd;
 }
-
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr) {
+    // if (arr.length === 0) return [];
+    
+    // console.log("initial arr:", arr);
+    // let pivotIndex = pivot(arr);
+
+    // let leftArr = arr.slice(0, pivotIndex);
+    // console.log("leftArr", leftArr);
+    // if (leftArr.length >= 2) {
+    //     let left = quickSort(leftArr);
+    //     console.log("left", left);
+    // }
+    
+    // let rightArr = arr.slice(pivotIndex);
+    // if (rightArr.length >= 2) {
+    //     let right =  quickSort(rightArr);
+    //     console.log("right", right);  
+    }
+}
 
 module.exports =  {quickSort, pivot};
+
+let a = quickSort([0, -10, 7, 4, -3]);
+console.log(a);
+// console.log(quickSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+// console.log(quickSort([0, 0, 7, 4, -2, 3, 0, 2])); // [-10, 0, 4, 7]
+// console.log(quickSort([0, -10, 7, 4, -3])); // [-10, 0, 4, 7]
+// console.log(quickSort([1, 2, 3])); // [1, 2, 3]
+// console.log(quickSort([]));
+
+// let nums = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
+// let nums = [4, 3, 5, 43, 232, 4];
+
+// quickSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34,
+                //   35, 43, 67, 75, 232, 232, 453, 546, 4342]
